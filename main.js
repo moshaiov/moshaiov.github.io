7f319ef2f2dabc24bc2afaf33d6fa2552db2ad47
moshaiov <92744307+moshaiov@users.noreply.github.com>
2021-11-08 04:33:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,6 @@ function show_hide(id) {
   }
 }
 
-function dark_light_button_fn() {
-  var url = window.location.href.split('?')[0];
-  var y = document.getElementById('dark_mode');
-  if (y.innerHTML==="dark mode")
-    url = url + "?dark=true";
-  window.location = url;
-
-}
-
 function toggle_dark_mode() {
   var x = document.body;
   x.classList.toggle("dark");
@@ -31,11 +22,15 @@ function toggle_dark_mode() {
     y.innerHTML="light mode";
 }
 
+function dark_light_button_fn() {
+  toggle_dark_mode();
+}
+
 function automatic_dark_mode()
 {
   let searchParams = new URLSearchParams(window.location.search);
   if(searchParams.has('dark') && searchParams.get('dark')==="true")
-    toggle_dark_mode();
+    dark_light_button_fn();
 }
 
 function mode_link(url)

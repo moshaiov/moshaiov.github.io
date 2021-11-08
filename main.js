d41210772474124736d2fc97475f79f187bda703
moshaiov <92744307+moshaiov@users.noreply.github.com>
2021-11-08 03:09:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -31,5 +31,23 @@ function automatic_dark_mode()
 {
   let searchParams = new URLSearchParams(window.location.search);
   if(searchParams.has('dark') && searchParams.get('dark')==="true")
-    dark_mode();
+  {
+    var x = document.body;
+  x.classList.toggle("dark");
+  var buttons = document.getElementsByTagName('button');
+  for (let i = 0; i < buttons.length; i++) {
+      let button = buttons[i];
+      button.classList.toggle("dark")
+  }
+  var hrs = document.getElementsByTagName('hr');
+  for (let i = 0; i < hrs.length; i++) {
+      let hr = hrs[i];
+      hr.classList.toggle("dark")
+  }
+  var y = document.getElementById('dark_mode');
+  if (y.innerHTML==="light mode")
+    y.innerHTML="dark mode";
+  else
+    y.innerHTML="light mode";
+  }
 }

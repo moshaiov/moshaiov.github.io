b760cb592cb76d61282b2503a6ba6f9860b35a1e
moshaiov <92744307+moshaiov@users.noreply.github.com>
2021-12-01 06:42:08 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,18 +1,42 @@
-level_1_starting_pieces = {
-     a:"vacant",
-     b:"king",
-     c:"vacant",
-     d:"pawn",
-     e:"pawn",
-     f:"vacant",
-     g:"pawn",
-     h:"vacant",
-     i:"vacant",
-     j:"vacant",
-     k:"vacant",
-     l:"vacant",
-     m:"vacant"
-};
+//todo:
+//select level
+// undo button
+// be able to change selection color
+// responsive
+// cheating buttons : is winnable, how many winnable moves
+
+const levels = [
+{
+a:"vacant",
+b:"king",
+c:"vacant",
+d:"pawn",
+e:"pawn",
+f:"vacant",
+g:"pawn",
+h:"vacant",
+i:"vacant",
+j:"vacant",
+k:"vacant",
+l:"vacant",
+m:"vacant"
+},
+{
+a:"vacant",
+b:"pawn",
+c:"vacant",
+d:"pawn",
+e:"pawn",
+f:"vacant",
+g:"pawn",
+h:"vacant",
+i:"vacant",
+j:"vacant",
+k:"vacant",
+l:"vacant",
+m:"king"
+}
+]
 
 xy_dict = {a:[0,0],b:[2,0],c:[4,0],
            d:[1,1],e:[3,1],
@@ -20,19 +44,23 @@ xy_dict = {a:[0,0],b:[2,0],c:[4,0],
            i:[1,3],j:[3,3],
            k:[0,4],l:[2,4],m:[4,4]}
 
+grid = {};
+for(key in xy_dict){grid[xy_dict[key]]="";}
+
 grid_size = 5
 var pixels = 200
-var king_color = "orange"
-var pawn_color = "teal"
-var vacant_color = "brown"
-var clicked_color = "red"
+color_dict = {king:"gray",pawn:"teal",vacant:"brown",clicked:"red"}
+
 
 canvas=document.getElementById('canvas');
 ctx=canvas.getContext('2d');
+output=document.getElementById('output');
+
+function isEqual(pointA,pointB) {
+  return pointA[0]===pointB[0] && pointA[1]===pointB[1]
+}
 
 function isGridPoint(point) {
-  grid = {};
-  for(key in xy_dict){grid[xy_dict[key]]="";} /* Transform the array in a dict */
   return point in grid;
 }
 
@@ -42,19 +70,18 @@ function pixelToGrid(x,y) {
   return [x,y]
 }
 
-function middle(start,end) {
-  xMid=(start[0]+end[0])/2
-  yMid=(start[1]+end[1])/2
+function middle(pointA,pointB) {
+  xMid=(pointA[0]+pointB[0])/2
+  yMid=(pointA[1]+pointB[1])/2
   return [xMid,yMid]
 }
 
-//start,end points given as [x,y].
-function isValidBoardJump(start,end) {
-  if(!(isGridPoint(start) && isGridPoint(end))) {return false}
-  //checking for same point or full diagonal. otherwise check if midpoint in grid.
-  if(start[0]===end[0] && start[1]===end[1]) {return false}
-  if(Math.abs(end[0]-start[0])+Math.abs(end[1]-start[1])===8) {return false}
+function isFullDiagonal(pointA,pointB) {
+  return 8 === Math.abs(pointB[0]-pointA[0]) + Math.abs(pointB[1]-pointA[1])
+}
 
+function isValidBoardJump(start,end) {
+  if( !isGridPoint(start) || !isGridPoint(end) || isEqual(start,end) || isFullDiagonal(start,end) ) {return false}
   return isGridPoint(middle(start,end))
 }
 
@@ -63,104 +90,106 @@ function getMousePos(canvas, evt) {
   return { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
 }
 
-function draw_square(point,color) {
+function drawSquare(point,color) {
   ctx.fillStyle=color
   ctx.fillRect(pixels*point[0]/grid_size,pixels*point[1]/grid_size,pixels/grid_size,pixels/grid_size)
 }
 
 function markSquare(point) {
-  ctx.fillStyle=clicked_color
+  ctx.fillStyle=color_dict['clicked']
   len = pixels/(2*grid_size)
-  ctx.fillRect(pixels*point[0]/grid_size+len/2,pixels*point[1]/grid_size+len/2,len,len)
+  ctx.fillRect((pixels*point[0])/grid_size+len/2,(pixels*point[1])/grid_size+len/2,len,len)
 }
 
-function get_color_by_piece(piece) {
-  if(piece=="king")
-    return king_color
-  if(piece=="pawn")
-    return pawn_color
-  if(piece=="vacant")
-    return vacant_color
+function getColor(piece) {
+  return color_dict[piece]
 }
 
-
-function getNumPawns(starting_pieces) {
+function getNumPawns(starting_board) {
   ret=0
-  for(var key in starting_pieces) {
-    if(starting_pieces[key]==="pawn") {
+  for(var key in starting_board) {
+    if(starting_board[key]==="pawn") {
       ret=ret+1
     }
   }
   return ret
 }
 
-// Define a class like this
-function Game(starting_pieces){
-   // Add object properties like this
-   this.pieces = starting_pieces;
-   this.selected = "none"
-   this.numPawns = getNumPawns(starting_pieces)
+//game class
+function Game(starting_board){
+   this.board = starting_board;
+   this.selected = "none" //either none, or point
+   this.numPawns = getNumPawns(starting_board)
 }
 
-// Add methods like this.  All Person objects will be able to invoke this
-Game.prototype.draw = function(){
-  console.log("drawing game");
-  for (var key in this.pieces) {
-      draw_square(xy_dict[key],get_color_by_piece(this.pieces[key]))
+Game.prototype.draw = function() {
+  for(var key in this.board) {
+      drawSquare(xy_dict[key],getColor(this.board[key]))
   }
   if(!(this.selected==="none")) {
     markSquare(this.selected)
   }
-};
+}
 
-Game.prototype.getPoint = function(point) {
+Game.prototype.getPiece = function(point) {
   for(var key in xy_dict) {
-    if(xy_dict[key][0]===point[0] && xy_dict[key][1]===point[1]) {
-      return this.pieces[key]
+    if(isEqual(xy_dict[key],point)) {
+      return this.board[key]
     }
   }
 }
 
-Game.prototype.setPoint = function(point,piece) {
+Game.prototype.setPiece = function(point,piece) {
   for(var key in xy_dict) {
-    if(xy_dict[key][0]===point[0] && xy_dict[key][1]===point[1]) {
-      this.pieces[key] = piece
+    if(isEqual(xy_dict[key],point)) {
+      this.board[key] = piece
     }
   }
 }
-
 
 Game.prototype.validSelection = function(point) {
-  return (this.getPoint(point)==="king" || this.getPoint(point)==="pawn")
+  return (this.getPiece(point)==="king" || this.getPiece(point)==="pawn")
 }
 
-//start and end given as [x,y].
 Game.prototype.isValidJump = function(start,end) {
-  if(!isValidBoardJump(start,end)) {return false}
-  return (!(this.getPoint(start)==="vacant")) && this.getPoint(end)==="vacant" && this.getPoint(middle(start,end))==="pawn"
+  return isValidBoardJump(start,end) && !(this.getPiece(start)==="vacant") && this.getPiece(end)==="vacant" && this.getPiece(middle(start,end))==="pawn"
 }
 
 //jump assumed valid
 Game.prototype.makeJump = function(start,end) {
   this.numPawns = this.numPawns - 1
-  this.setPoint(end, this.getPoint(start))
-  this.setPoint(start,"vacant")
-  this.setPoint(middle(start,end),"vacant")
+  this.setPiece(end, this.getPiece(start))
+  this.setPiece(start,"vacant")
+  this.setPiece(middle(start,end),"vacant")
 }
 
-Game.prototype.click = function(point){
-  console.log("click")
-  console.log(point)
+Game.prototype.checkWon = function() {
+  //we don't allow eating the king anyway
+  return this.numPawns===0
+}
 
-  //case A: no piece was selected
+Game.prototype.numMoves = function() {
+  ret=0
+  for(var key1 in xy_dict) {
+    for(var key2 in xy_dict) {
+      if(this.isValidJump(xy_dict[key1],xy_dict[key2])) {
+        ret = ret + 1
+      }
+    }
+  }
+  return ret
+}
+
+Game.prototype.checkLost = function() {
+  return this.numMoves()===0 && !this.checkWon()
+}
+
+Game.prototype.click = function(point) {
   if(this.selected==="none") {
     if(this.validSelection(point)) {
       this.selected=point
-      this.draw()
     }
   }
-
-  //case B: a piece was selected
   else {
     start = this.selected
     end = point
@@ -168,19 +197,28 @@ Game.prototype.click = function(point){
     if(this.isValidJump(start,end)) {
       this.makeJump(start,end)
     }
-    this.draw()
   }
 
+  this.draw()
+  if(this.checkWon()) {
+    setTimeout(() => {output.value = "you won!"},250)
+  }
+  if(this.checkLost()) {
+    setTimeout(() => {output.value = "no possible moves :("},500)
+  }
 }
 
-// Instantiate new objects with 'new'
-var game = new Game(level_1_starting_pieces);
-
-// Invoke methods like this
+var level=1
+var game = new Game(levels[level]);
 game.draw();
 
 canvas.addEventListener('click', function(evt) {
   var mousePos = getMousePos(canvas, evt);
   game.click(pixelToGrid(mousePos.x,mousePos.y))
-  //console.log(pixelToGrid(mousePos.x,mousePos.y))
 });
+
+function changeColor(x) {
+    val=document.getElementById(x).value;
+    color_dict[x]=val
+    game.draw()
+}

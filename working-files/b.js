a26ef3d4412082fa19e01d0bedc44a575155f68e
moshaiov <92744307+moshaiov@users.noreply.github.com>
2022-01-12 19:34:23 +0200

    Update and rename mm.html to b.js

---
+++ working-files/b.js
@@ -0,0 +1,8 @@
+document.write('\
+\
+    <title>moshaiov</title>\
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\
+    <script type="text/x-mathjax-config"> MathJax.Hub.Config({ tex2jax: { inlineMath: [ ['$','$'], ["\\(","\\)"] ], processEscapes: true }});</script>\
+    <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>\
+\
+');

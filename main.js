af0fbe2f5dfa2786908ef23ee0f167d37ae667b6
moshaiov <92744307+moshaiov@users.noreply.github.com>
2022-04-03 02:55:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ function ToggleDarkMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+function AddQedSymbols() {
+   var els = document.getElementsByClassName("qed");
+   for(var i = 0; i < els.length; i++) {
+      var x = els[i];
+      x.insertAdjacentHTML('afterend', '◆');
+   }
+}
